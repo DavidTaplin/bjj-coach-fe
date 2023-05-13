dd3cafac59c53ef2e173a55e3064002f4b077e66
Joe Letner <jrletner@gmail.com>
2023-05-13 12:23:58 -0500

    Fixed API path & removed name requirement in form

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,20 +3,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   private API_URL = 'http://localhost:3000/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   signUp(userData: any): Observable<any> {
-    return this.http.post<any>(`${this.API_URL}/signup`, userData);
+    // return this.http.post<any>(`${this.API_URL}/signup`, userData);
+    return this.http.post<any>(`${this.API_URL}`, userData);
   }
 }
-
-
-
-
-
-
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,29 +6,35 @@ import { ApiService } from '../api.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
   signUpForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private apiService: ApiService, private router: Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.signUpForm = this.formBuilder.group({
-      name: ['', Validators.required],
+      //name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   onSubmit() {
+    console.log(this.signUpForm.value);
     if (this.signUpForm.invalid) {
+      console.log('INVALID');
       return;
     }
 
     this.apiService.signUp(this.signUpForm.value).subscribe(
-      result => console.log(result),
-      error => console.log(error)
+      (result) => console.log(result),
+      (error) => console.log(error)
     );
   }
 }
