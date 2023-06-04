02a587e31ad57d2298ed2107cc0fc881b8dfb0cf
DavidTaplin <DTaplin616@live.com>
2023-06-03 20:00:34 -0500

    movelist WIP
    form now adds to array and shows up on the html. Doesn't save once refresh it is gone

--- src/app/movelist/movelist.component.ts
+++ src/app/movelist/movelist.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 interface Move {
-  id: number,
-  name: string,
-  position: string
+  id: number;
+  name: string;
+  position: string;
 }
 
 @Component({
@@ -11,15 +12,36 @@ interface Move {
   templateUrl: './movelist.component.html',
   styleUrls: ['./movelist.component.css']
 })
-export class MovelistComponent {
+export class MovelistComponent implements OnInit {
 
   moves: Move[] = [
-    {id: 1, name: "Kimura", position: "closed guard"},
-    {id: 2, name: "Cross Collar Choke", position: "closed guard"},
-    {id: 3, name: "Arm Triangle", position: "closed guard"},
+    { id: 1, name: "Kimura", position: "closed guard" },
+    { id: 2, name: "Cross Collar Choke", position: "closed guard" },
+    { id: 3, name: "Arm Triangle", position: "closed guard" },
+  ];
 
+  moveForm: FormGroup | null = null; // Initialize with null
 
+  constructor(private formBuilder: FormBuilder) { }
 
-  ]
+  ngOnInit() {
+    this.moveForm = this.formBuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      position: ['', Validators.required]
+    });
+  }
 
+  addMove(): void {
+    if (this.moveForm && this.moveForm.valid) {
+      const newMove: Move = {
+        id: this.moveForm.controls['id'].value,
+        name: this.moveForm.controls['name'].value,
+        position: this.moveForm.controls['position'].value
+      };
+
+      this.moves.push(newMove);
+      this.moveForm.reset();
+    }
+  }
 }
