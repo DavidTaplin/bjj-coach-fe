aa939d0a527a5054f3d0bd439a8767c6ebeb739c
DavidTaplin <DTaplin616@live.com>
2023-06-07 20:00:26 -0500

    updated form for move list
    took off the id input and added grip input and updated the ts files to reflect these changes. form works and adds changes

--- src/app/movelist/movelist.component.ts
+++ src/app/movelist/movelist.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 interface Move {
-  id: number;
   name: string;
   position: string;
   grip: string;
@@ -16,9 +15,9 @@ interface Move {
 export class MovelistComponent implements OnInit {
 
   moves: Move[] = [
-    { id: 1, name: "Kimura", position: "closed guard", grip:'collar and elbow' },
-    { id: 2, name: "Cross Collar Choke", position: "closed guard", grip: "collar" },
-    { id: 3, name: "scissor sweep", position: "closed guard", grip: "collar and elbow"},
+    {  name: "Kimura", position: "closed guard", grip:'collar and elbow' },
+    {  name: "Cross Collar Choke", position: "closed guard", grip: "collar" },
+    {  name: "scissor sweep", position: "closed guard", grip: "collar and elbow"},
   ];
 
   moveForm: FormGroup | null = null; // Initialize with null
@@ -27,7 +26,6 @@ export class MovelistComponent implements OnInit {
 
   ngOnInit() {
     this.moveForm = this.formBuilder.group({
-      id: ['', Validators.required],
       name: ['', Validators.required],
       position: ['', Validators.required],
       grip: ['', Validators.required]
@@ -37,7 +35,7 @@ export class MovelistComponent implements OnInit {
   addMove(): void {
     if (this.moveForm && this.moveForm.valid) {
       const newMove: Move = {
-        id: this.moveForm.controls['id'].value,
+
         name: this.moveForm.controls['name'].value,
         position: this.moveForm.controls['position'].value,
         grip: this.moveForm.controls['grip'].value
